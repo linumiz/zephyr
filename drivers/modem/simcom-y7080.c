4f0e9b07b40315fda2b6ae8c2be69a9016ce7b9e
Karthikeyan Krishnasamy <karthikeyan@linumiz.com>
2023-04-04 17:07:46 +0530

    drivers: modem: simcom driver abstraction
    add initial setup for simcom driver abstraction
    
    Signed-off-by: Karthikeyan Krishnasamy <karthikeyan@linumiz.com>

--- drivers/modem/simcom-y7080.c
+++ drivers/modem/simcom-y7080.c
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2021 metraTec GmbH
  * Copyright (C) 2023 Linumiz
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -9,11 +8,12 @@
 
 #include "simcom-xxxx.h"
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(modem_sim7080, CONFIG_MODEM_LOG_LEVEL);
+LOG_MODULE_REGISTER(modem_Y7080, CONFIG_MODEM_LOG_LEVEL);
 
-#include "simcom-sim70801.h"
+#include "simcom-y7080.h"
 
-#if 0
+#define GNSS_SKIP_BYTES 	5
+
 int offload_connect_cmd(struct modem_socket *sock, const struct sockaddr *addr, char *buf)
 {
 	char *protocol_type;
@@ -34,7 +34,7 @@ int offload_connect_cmd(struct modem_socket *sock, const struct sockaddr *addr, 
 	protocol = (sock->type == SOCK_STREAM) ? 6 : 17;
 	receive_ctrl = 1; /* Receive control */
 
-	ret = snprintk(buf, sizeof(buf), "AT+NSOCR= %s, %d, %d, %d, %d, %s", protocol_type, protocol, 0, sock->sock_fd,
+	ret = snprintk(buf, sizeof(buf), "AT+NSOCR= \"%s\", %d, %d, %d, %d, \"%s\"", protocol_type, protocol, 0, sock->sock_fd,
 			receive_ctrl, af_type);
 	if (ret < 0) {
 		LOG_ERR("Failed to build connect command. ID: %d, FD: %d", sock->id, sock->sock_fd);
@@ -43,7 +43,6 @@ int offload_connect_cmd(struct modem_socket *sock, const struct sockaddr *addr, 
 
 	return 0;
 }
-#endif
 
 static float get_float_from_str(char *buf)
 {
@@ -59,69 +58,91 @@ int parse_gnssinfo(char *gps_buf, struct simcom_gnss_data *gnss_data)
 {
 	char *saveptr;
 	int ret;
+	char *buf;
 	int32_t number, fraction;
 
-	char *run_status = gnss_get_next_param(gps_buf, ",", &saveptr);
-	if (run_status == NULL) {
-		goto error;
-	} else if (*run_status != '1') {
-		goto error;
+	for(int i = 0; i < GNSS_SKIP_BYTES; i++){
+		buf = strchr(gps_buf, ',');
+		if(buf == NULL){
+			return -1;
+		}
+		gps_buf = buf + 1;
 	}
 
-	char *fix_status = gnss_get_next_param(NULL, ",", &saveptr);
-	if (fix_status == NULL) {
-		goto error;
-	} else if (*fix_status != '1') {
-		goto error;
-	}
-
-	char *utc = gnss_get_next_param(NULL, ",", &saveptr);
-	if (utc == NULL) {
-		goto error;
-	}
-
-	char *lat = gnss_get_next_param(NULL, ",", &saveptr);
+	char *lat = gnss_get_next_param(gps_buf, ",", &saveptr);
 	if (lat == NULL) {
-		goto error;
+		lat = "0";
+	}
+
+	char *lat_dir = gnss_get_next_param(NULL, ",", &saveptr);
+	if(lat_dir == NULL) {
+		lat_dir = "0";
 	}
 
 	char *lon = gnss_get_next_param(NULL, ",", &saveptr);
 	if (lon == NULL) {
-		goto error;
+		lon = "0";
+	}
+
+	char *lon_dir = gnss_get_next_param(NULL, ",", &saveptr);
+	if(lon_dir == NULL) {
+		lon_dir = "0";
+	}
+
+	char *date = gnss_get_next_param(NULL, ",", &saveptr);
+	if(date == NULL){
+		date = "0";
+	}
+
+	char *utc_time = gnss_get_next_param(NULL, ",", &saveptr);
+	if(utc_time == NULL){
+		utc_time = "0";
 	}
 
 	char *alt = gnss_get_next_param(NULL, ",", &saveptr);
-	char *speed = gnss_get_next_param(NULL, ",", &saveptr);
-	char *course = gnss_get_next_param(NULL, ",", &saveptr);
+	if(alt == NULL){
+		alt = "0";
+	}
 
-	/* discard fix mode and reserved*/
-	gnss_skip_param(&saveptr);
+	char *speed = gnss_get_next_param(NULL, ",", &saveptr);
+	if(speed == NULL){
+		speed = "0";
+	}
+
+	char *course = gnss_get_next_param(NULL, ",", &saveptr);
+	if(course == NULL){
+		course = "0";
+	}
+
 	gnss_skip_param(&saveptr);
 
 	char *hdop = gnss_get_next_param(NULL, ",", &saveptr);
 	if (hdop == NULL) {
-		goto error;
+		hdop = "0";
 	}
 
 	gnss_data->lat = get_float_from_str(lat);
-        gnss_data->lon = get_float_from_str(lon);
-        if (alt) {
-                gnss_data->alt =  get_float_from_str(alt);
-        } else {
-                gnss_data->alt = 0;
-        }
+	gnss_data->lat_dir = lat_dir[0];
+	gnss_data->lon = get_float_from_str(lon) ;
+	gnss_data->lon_dir = lon_dir[0];
+	if (alt) {
+		gnss_data->alt =  get_float_from_str(alt);;
+	} else {
+		gnss_data->alt = 0;
+	}
 
-	strcpy(gnss_data->utc, utc);
+	strcpy(gnss_data->date, date);
+
+	strcpy(gnss_data->utc, utc_time);
+
 	if(speed)
-                gnss_data->speed = get_float_from_str(speed);
+		gnss_data->speed = get_float_from_str(speed);
 
 	gnss_data->hdop = get_int_from_str(hdop);
 
 	if(course)
-                gnss_data->cog = get_int_from_str(course);
+		gnss_data->cog = get_int_from_str(course);
 
 	return 0;
-error:
-	memset(&gnss_data, 0, sizeof(gnss_data));
-	return -1;
 }
+
