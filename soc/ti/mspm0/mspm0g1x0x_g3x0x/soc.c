29c9a2ad402fe934dcb5e3376bfb96de4789c005
Parthiban Nallathambi <parthiban@linumiz.com>
2024-11-28 10:03:31 +0530

    soc: ti/mspm0: remove init delay cycles
    delay cycles in the init is not required and not documented.
    Remove them.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- soc/ti/mspm0/mspm0g1x0x_g3x0x/soc.c
+++ soc/ti/mspm0/mspm0g1x0x_g3x0x/soc.c
@@ -10,9 +10,6 @@
 
 static int ti_mspm0g_init(void)
 {
-	/* Allow delay time to settle */
-	delay_cycles(POWER_STARTUP_DELAY);
-
 	/* Low Power Mode is configured to be SLEEP0 */
 	DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0);
 
