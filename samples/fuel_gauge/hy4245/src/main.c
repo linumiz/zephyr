466083cc38463c4aa2d1a77b916796a13396d41c
Saravanan Sekar <saravanan@linumiz.com>
2025-03-04 19:24:56 +0530

    drivers: fuelgauge: Add support for data flash access
    HY4245 fuel guage has data flash used to store calibration data,
    battery configuration data, charge control etc as persistant.
    
    Supports multiple bank(sub class) and each bank has multiple block
    (each block has 32byte). Add interface to read and write data flash.
    
    Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>

--- samples/fuel_gauge/hy4245/src/main.c
+++ samples/fuel_gauge/hy4245/src/main.c
@@ -14,6 +14,7 @@ int main(void)
 {
 	const struct device *const dev = DEVICE_DT_GET_ANY(ht_hy4245);
 	int ret = 0;
+	uint8_t data[32] = {0};
 
 	if (dev == NULL) {
 		printk("\nError: no device found.\n");
@@ -33,18 +34,54 @@ int main(void)
 		return 0;
 	}
 
-	while (1) {
+	ret = hy4245_enable_flash_access(dev);
+	if (ret) {
+		printk("enable flash access err %d\n", ret);
+		return 0;
+	}
 
+	ret = hy4245_set_calibration_mode(dev);
+	if (ret) {
+		printk("set calib mode err %d\n", ret);
+		return 0;
+	}
+
+	ret =hy4245_access_flash_data(dev, 0x3, 1,
+			data, sizeof(data), true);
+	if (ret) {
+		printk("read flash data err %d\n", ret);
+		return 0;
+	}
+
+	printk("read flash data\n", ret);
+	for(int i = 0; i<sizeof(data); i++)
+		printk("%x\t", data[i]);
+
+#if 0
+	data[5] = 0xfa;
+
+	ret =hy4245_access_flash_data(dev, 0x3, 1,
+			data, sizeof(data), false);
+	if (ret) {
+		printk("write flash data err %d\n", ret);
+		return 0;
+	}
+#endif
+
+	while (1) {
 		fuel_gauge_prop_t props[] = {
 			FUEL_GAUGE_RUNTIME_TO_EMPTY,
 			FUEL_GAUGE_RUNTIME_TO_FULL,
 			FUEL_GAUGE_VOLTAGE,
 			FUEL_GAUGE_REMAINING_CAPACITY,
+			FUEL_GAUGE_FULL_CHARGE_CAPACITY,
 			FUEL_GAUGE_CHARGE_VOLTAGE,
+			FUEL_GAUGE_CHARGE_CURRENT,
 		};
 
 		union fuel_gauge_prop_val vals[ARRAY_SIZE(props)];
 
+		memset(vals, 0, sizeof(vals));
 		ret = fuel_gauge_get_props(dev, props, vals, ARRAY_SIZE(props));
 		if (ret < 0) {
 			printk("Error: cannot get properties\n");
@@ -53,11 +90,13 @@ int main(void)
 
 			printk("Time to full %d minutes\n", vals[1].runtime_to_full);
 
-			printk("Voltage %d\n uV", vals[2].voltage);
+			printk("Voltage %d uV\n", vals[2].voltage);
 
-			printk("Remaning capacity %u\n", vals[3].remaining_capacity);
+			printk("Remaning capacity %u uAh\n", vals[3].remaining_capacity);
+			printk("full capacity %u uAh\n", vals[4].full_charge_capacity);
 
-			printk("Charge voltge %u\n uV", vals[4].chg_voltage);
+			printk("Charge voltge %u uV\n", vals[5].chg_voltage);
+			printk("Charge voltge %u uA\n", vals[6].chg_current);
 		}
 
 		k_sleep(K_MSEC(5000));
