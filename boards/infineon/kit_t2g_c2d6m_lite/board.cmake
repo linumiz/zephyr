8d94e13b7b38ace10d04c7af6bbad4522b9881f2
Karthikeyan Krishnasamy <karthikeyan@linumiz.com>
2025-10-08 15:50:43 +0530

    boards: infineon: kit_t2g_c2d6m_lite: fix flash issues
    default flashing is using openocd, fix using openocd from ifx
    vendor implementation, for now it have disabled cortex-m0 config
    
    flash command for ifx below, which use openocd from ifx modbus toolsbox version
    
    west flash --openocd /opt/Tools/ModusToolboxProgtools-1.5/openocd/bin/openocd
    --openocd-search /opt/Tools/ModusToolboxProgtools-1.5/openocd/scripts
    
    Signed-off-by: Karthikeyan Krishnasamy <karthikeyan@linumiz.com>

--- boards/infineon/kit_t2g_c2d6m_lite/board.cmake
+++ boards/infineon/kit_t2g_c2d6m_lite/board.cmake
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Connect to CM0P core.
-board_runner_args(openocd "--target-handle=cat1c.cpu.cm0")
+#board_runner_args(openocd "--target-handle=cat1c.cpu.cm0")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
