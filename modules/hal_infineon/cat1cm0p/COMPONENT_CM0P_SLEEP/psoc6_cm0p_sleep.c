7364c5102f5ff39a1fd54854f53df2370b5eb894
Pranati-Hota_GHIFX <Pranati.hota@infineon.com>
2026-02-19 15:58:25 +0530

    soc: infineon: Add support for TVIIBE4M Soc Family and KIT_T2G_B_E_LITE Added soc support for CYT2BL family. Verified Basic Tests. Added interrupt support and verified with uart and watchdog. Added Board Support for KIT_T2G_B_E_LITE. Signed-off-by: Pranati-IFX <Pranati.hota@infineon.com>

---
+++ modules/hal_infineon/cat1cm0p/COMPONENT_CM0P_SLEEP/psoc6_cm0p_sleep.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company) or
+ * an affiliate of Cypress Semiconductor Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+#include "cy_device_headers.h"
+
+#if defined(CY_DEVICE_PSOC6A256K) || defined(CY_DEVICE_PSOC6A512K) || defined(CY_DEVICE_PSOC6A2M)
+
+#if defined(__APPLE__) && defined(__clang__)
+__attribute__ ((__section__("__CY_M0P_IMAGE,__cy_m0p_image"), used))
+#elif defined(__GNUC__) || defined(__ARMCC_VERSION)
+__attribute__ ((__section__(".cy_m0p_image"), used))
+#elif defined(__ICCARM__)
+#pragma  location=".cy_m0p_image"
+#else
+#error "An unsupported toolchain"
+#endif
+const uint8_t cy_m0p_image[] = {
+    #include <psoc6_cm0p_sleep.bin.inc>
+};
+
+#endif /* defined(CY_DEVICE_PSOC6A256K) || defined(CY_DEVICE_PSOC6A512K) || defined(CY_DEVICE_PSOC6A2M) */
