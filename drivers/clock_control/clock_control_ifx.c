e8180a0f949e03fb6997be5b2d4cf7bcd2018af3
Karthikeyan Kalamani <karthikeyan@linumiz.com>
2026-03-03 17:44:38 +0530

    drivers: clock_control_ifx: set slow clock frequency divider
    CLK_SLOW is supplied from CLK_HF0, by default CLK_HF0 is set to
    max freq 200 MHz, CLK_SLOW max frequency is 100 MHz. Setting
    divider value to 1 gives 100MHz (CLK_SLOW), so enable slow clk
    divider for CAT1C series too.
    
    Signed-off-by: Karthikeyan Kalamani <karthikeyan@linumiz.com>

--- drivers/clock_control/clock_control_ifx.c
+++ drivers/clock_control/clock_control_ifx.c
@@ -69,10 +69,7 @@ static int ifx_clock_init(const struct device *dev)
 	}
 #endif
 
-#if defined (CONFIG_SOC_DIE_CYT2BL)
-	/* clock slow 80 Mhz */
 	Cy_SysClk_ClkSlowSetDivider(1);
-#endif
 
 	int i;
 	for (i = 0; i < SRSS_NUM_CLKPATH; i++) {
