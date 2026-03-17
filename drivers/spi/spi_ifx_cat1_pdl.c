b9ef4c18e05e49fde2d55ed5d066685a90c606c8
shreyasifx <shreyasg.khanapur@infineon.com>
2026-03-17 12:30:51 +0530

    drivers:spi: Add 1:1 trigger for CYT2B7
    Add 1:1 trigger mapping for SPI to DMA.
    Update few conditional compilation related
    to triggers.
    
    Signed-off-by : Shreyas G Khanapur ShreyasG.Khanapur@infineon.com

--- drivers/spi/spi_ifx_cat1_pdl.c
+++ drivers/spi/spi_ifx_cat1_pdl.c
@@ -101,7 +101,7 @@ struct ifx_cat1_spi_data {
 #ifdef CONFIG_IFX_CAT1_SPI_DMA
 	struct ifx_cat1_dma_stream dma_rx;
 	struct ifx_cat1_dma_stream dma_tx;
-#if !defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C)
+#if !defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C) && !defined(CONFIG_SOC_SERIES_TVII_B_E)
 	en_peri0_trig_input_pdma0_tr_t spi_rx_trigger;
 	en_peri0_trig_output_pdma0_tr_t dma_rx_trigger;
 #else
@@ -483,7 +483,7 @@ static int ifx_cat1_spi_init(const struct device *dev)
 	data->resource.block_num = ifx_cat1_uart_get_hw_block_num(config->reg_addr);
 
 #ifdef CONFIG_IFX_CAT1_SPI_DMA
-#if defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C)
+#if defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C) || defined(CONFIG_SOC_SERIES_TVII_B_E)
 	/* spi_rx_trigger is initialized to TRIG_OUT_1TO1_1_SCB_RX_TO_PDMA10,
 	 * this is incremented by the resource.block_num to get the trigger for the selected SCB
 	 */
@@ -520,7 +520,7 @@ static int ifx_cat1_spi_init(const struct device *dev)
 		data->dma_tx.dma_cfg.dma_callback = dma_callback;
 	}
 
-#if defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C)
+#if defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C) || defined(CONFIG_SOC_SERIES_TVII_B_E)
 	Cy_TrigMux_Select(data->spi_rx_trigger, false, TRIGGER_TYPE_LEVEL);
 #else
 	Cy_TrigMux_Connect(data->spi_rx_trigger, data->dma_rx_trigger, false, TRIGGER_TYPE_LEVEL);
@@ -574,6 +574,13 @@ static int ifx_cat1_spi_init(const struct device *dev)
 #else
 #define SPI_DMA_TRIGGERS(index)                                                                  \
 	.spi_rx_trigger	= TRIG_OUT_1TO1_2_SCB_RX_TO_PDMA10,
+#endif
+
+#elif defined(CONFIG_SOC_SERIES_TVII_B_E)
+
+#if defined(CONFIG_SOC_DIE_CYT2B7)
+#define SPI_DMA_TRIGGERS(index)                                                                  \
+	.spi_rx_trigger	= TRIG_OUT_1TO1_8_SCB_RX_TO_PDMA10,
 #endif
 
 #else
@@ -704,7 +711,7 @@ static int ifx_cat1_spi_init(const struct device *dev)
 	static struct ifx_cat1_spi_data spi_cat1_data_##n = {                                      \
 		SPI_CONTEXT_INIT_LOCK(spi_cat1_data_##n, ctx),                                     \
 		SPI_CONTEXT_INIT_SYNC(spi_cat1_data_##n, ctx),                                     \
-		SPI_DMA_CHANNEL(n, tx, MEMORY_TO_PERIPHERAL, 1, 1)                                 \
+		SPI_DMA_CHANNEL(n, tx, MEMORY_TO_PERIPHERAL, 1, 1)                               \
 			SPI_DMA_CHANNEL(n, rx, PERIPHERAL_TO_MEMORY, 1, 1) SPI_DMA_TRIGGERS(n)     \
 				SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(n), ctx)               \
 					SPI_PERI_CLOCK_INIT(n)                                     \
@@ -932,7 +939,7 @@ static cy_rslt_t ifx_cat1_spi_int_frequency(const struct device *dev, uint32_t h
 	uint32_t peri_freq = Cy_SysClk_ClkHfGetFrequency(hfclk);
 #elif defined(CONFIG_SOC_FAMILY_INFINEON_CAT1C)
 	uint32_t peri_freq;
-	ret = clock_control_get_rate(DEVICE_DT_GET(DT_NODELABEL(clk_hf0)),
+	ret = clock_control_get_rate(DEVICE_DT_GET(DT_NODELABEL(clk_hf2)),
 	                              NULL, &peri_freq);
 	if (ret < 0) {
 		LOG_ERR("Failed to get HF clock frequency");
