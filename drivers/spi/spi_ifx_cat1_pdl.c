e4a68e41f0ac523c2b4a1f978a75edd44fbd3e3d
Pranati-Hota_GHIFX <Pranati.hota@infineon.com>
2026-03-19 00:43:55 +0530

    drivers: spi: ifx_cat1: fix DMA trigger configuration for TVII_B_E
    Update conditional compilation around trigger types and TrigMux configuration, and add the correct TVII_B_E trigger constant for
    CYT2BL so SPI DMA initialization uses the proper routing.
    
    Signed-off-by: Pranati Hota <pranati.hota@infineon.com>

--- drivers/spi/spi_ifx_cat1_pdl.c
+++ drivers/spi/spi_ifx_cat1_pdl.c
@@ -571,6 +571,11 @@ static int ifx_cat1_spi_init(const struct device *dev)
 #if defined(CONFIG_SOC_SERIES_CYT4DN)
 #define SPI_DMA_TRIGGERS(index)                                                                  \
 	.spi_rx_trigger	= TRIG_OUT_1TO1_1_SCB_RX_TO_PDMA10,
+#elif defined(CONFIG_SOC_SERIES_TVII_B_E)
+#if defined(CONFIG_SOC_DIE_CYT2BL)
+#define SPI_DMA_TRIGGERS(index) 								\
+        .spi_rx_trigger = TRIG_OUT_1TO1_8_SCB_RX_TO_PDMA10,
+#endif
 #else
 #define SPI_DMA_TRIGGERS(index)                                                                  \
 	.spi_rx_trigger	= TRIG_OUT_1TO1_2_SCB_RX_TO_PDMA10,
