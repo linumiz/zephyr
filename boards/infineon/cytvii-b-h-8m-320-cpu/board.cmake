befd757a2b7f15475f421b7a7bfe969815485126
Karthikeyan Krishnasamy <karthikeyan@linumiz.com>
2026-03-30 15:48:46 +0530

    boards: infineon: cytvii-b-h-8m-320-cpu: add jlink flash support
    added flashing support using SEGGER Jlink
    
    Signed-off-by: Karthikeyan Krishnasamy <karthikeyan@linumiz.com>

--- boards/infineon/cytvii-b-h-8m-320-cpu/board.cmake
+++ boards/infineon/cytvii-b-h-8m-320-cpu/board.cmake
@@ -5,5 +5,7 @@
 
 # Connect to CM0P core
 board_runner_args(openocd "--target-handle=cat1c.cpu.cm0")
+board_runner_args(jlink "--device=CYT4BFCCHE" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
